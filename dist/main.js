94f8edb911f2510c5e3885e7ddbb33728db036b7
andrew.zolotaryov <andrew.zolotaryov@deel.com>
2025-02-12 01:24:18 +0200

    Fix: Enhance logging for terrakube folder processing and JSON loading in main.js

--- dist/main.js
+++ dist/main.js
@@ -59,12 +59,15 @@ function run() {
             const patterns = ['**/terrakube.json'];
             const globber = yield glob.create(patterns.join('\n'));
             core.info(`Changed Directory: ${githubActionInput.terrakubeFolder}`);
+            core.info(`Terrakube Folders List: ${githubActionInput.terrakubeFolder.split(" ")}`);
             try {
                 for (var _b = __asyncValues(globber.globGenerator()), _c; _c = yield _b.next(), !_c.done;) {
                     const file = _c.value;
                     core.info(`File ${file}`);
                     const terrakubeData = JSON.parse(yield (0, promises_1.readFile)(`${file}`, "utf8"));
                     core.info(`Loaded JSON: ${JSON.stringify(terrakubeData)}`);
+                    core.info(`TerrakubeFolder: ${githubActionInput.terrakubeFolder}`);
+
                     const workspaceFolder = path_1.default.basename(path_1.default.dirname(file));
                     core.info(`Folder ${workspaceFolder} change: ${githubActionInput.terrakubeFolder.split(" ").indexOf(workspaceFolder)}`);
                     const workspaceName = terrakubeData.workspace && terrakubeData.workspace.trim() !== ""
