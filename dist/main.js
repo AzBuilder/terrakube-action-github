5c7977787fe525f284745b01fccd1f2e0d884231
andrew.zolotaryov <andrew.zolotaryov@deel.com>
2025-02-12 00:53:39 +0200

    Fix: Refactor terrakube folder handling for improved clarity

--- dist/main.js
+++ dist/main.js
@@ -59,16 +59,17 @@ function run() {
             const patterns = ['**/terrakube.json'];
             const globber = yield glob.create(patterns.join('\n'));
             core.info(`Changed Directory: ${githubActionInput.terrakubeFolder}`);
+            const terrakubeFolders = githubActionInput.terrakubeFolder.split(" ");
             try {
                 for (var _b = __asyncValues(globber.globGenerator()), _c; _c = yield _b.next(), !_c.done;) {
                     const file = _c.value;
                     const terrakubeData = JSON.parse(yield (0, promises_1.readFile)(`${file}`, "utf8"));
                     const workspaceFolder = path_1.default.basename(path_1.default.dirname(file));
-                    core.info(`Folder ${workspaceFolder} change: ${githubActionInput.terrakubeFolder.split(" ").indexOf(workspaceFolder)}`);
+                    core.info(`Folder ${workspaceFolder} change: ${terrakubeFolders.indexOf(workspaceFolder)}`);
                     const workspaceName = terrakubeData.workspace && terrakubeData.workspace.trim() !== ""
                         ? terrakubeData.workspace : workspaceFolder;
                     //Folder with terrakube.json file change
-                    if (githubActionInput.terrakubeFolder.split(" ").indexOf(workspaceFolder) > -1) {
+                    if (terrakubeFolders.indexOf(workspaceFolder) > -1) {
                         core.startGroup(`Execute Workspace ${workspaceName}`);
                         console.debug(`Processing: ${file}`);
                         core.debug(`Loaded JSON: ${JSON.stringify(terrakubeData)}`);
